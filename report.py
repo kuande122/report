4938722e3a3f9e2bb3f554fcf0fcaa4b197e3edd
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-06 21:00:00 +0800

    Update report.py

--- report.py
+++ report.py
@@ -1,13 +1,7 @@
 __author__="teacher"
 import random
 import streamlit as st
-confirm_input = st.button('確認產生答案')
-if confirm_input:
- st.session_state.c = c = random.randint(2,99)
- st.session_state.start = 1
- st.session_state.end = 100
- st.session_state.start,st.session_state.end = 1,100
- st.write('c=', st.session_state.c)
+
  
 x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end)) 
 confirm_input2 = st.button('輸入確認')
@@ -24,4 +18,10 @@ if confirm_input2:
    st.write("輸入不合法,請重新輸入:")
   else:
    st.session_state.start=x
-
+confirm_input = st.button('確認產生答案')
+if confirm_input:
+ st.session_state.c = c = random.randint(2,99)
+ st.session_state.start = 1
+ st.session_state.end = 100
+ st.session_state.start,st.session_state.end = 1,100
+ st.write('c=', st.session_state.c)
