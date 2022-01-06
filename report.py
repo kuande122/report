46f0ab0bb3027e9aa41864722ed5b22d99bc1286
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-06 21:03:42 +0800

    Update report.py

--- report.py
+++ report.py
@@ -2,6 +2,10 @@ __author__="teacher"
 import random
 import streamlit as st
 
+
+# Session State also supports the attribute based syntax
+if 'key' not in st.session_state:
+    st.session_state.key = 'value'
  
 x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end)) 
 confirm_input2 = st.button('輸入確認')
@@ -19,6 +23,10 @@ if confirm_input2:
   else:
    st.session_state.start=x
 confirm_input = st.button('確認產生答案')
+if 'start' not in st.session_state:
+    st.session_state['start'] = 'value'
+if 'end' not in st.session_state:
+    st.session_state['end'] = 'value'  
 if confirm_input:
  st.session_state.c = c = random.randint(2,99)
  st.session_state.start = 1
