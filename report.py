172c506ca451a7b88e4f9416be623c324ca56e11
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-06 20:09:35 +0800

    Update report.py

--- report.py
+++ report.py
@@ -1,1 +1,25 @@
+import streamlit as st
+import pandas as pd               # 資料處理套件
+import matplotlib.pyplot as plt   # 資料視覺化套件
+import altair as alt
+import numpy as np
+from PIL import Image
+wang = pd.read_csv('wang.csv')
+wang.head(3)    # 顯示前3筆資料
+option = st.sidebar.selectbox( '選擇球隊？', ['中信兄弟', '統一7-Eleven獅', '味全龍', '樂天桃猿','富邦悍將'])
+if option == '中信兄弟':
+  image = Image.open('brothers.png')
+  st.image(image)
+elif option == '統一7-Eleven獅':
+  image = Image.open('unilion.png')
+  st.image(image)
+elif option == '味全龍':
+  image = Image.open('Dragons.png')
+  st.image(image)
+elif option == '樂天桃猿':
+  image = Image.open('Rakuten.png')
+  st.image(image)
+elif option == '富邦悍將':
+  image = Image.open('guardians.png')
+  st.image(image)
 
