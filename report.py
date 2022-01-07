026de0b1a91f8a1e749a153fe0c3900753618331
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 14:04:38 +0800

    Update report.py

--- report.py
+++ report.py
@@ -123,7 +123,7 @@ if option == '中信兄弟':
         plt.subplots_adjust(left=0.125,
                     bottom=0.2, 
                     right=0.9, 
-                    top=0.9, 
+                    top=1.0, 
                     wspace=0.2, 
                     hspace=0.9)
         st.pyplot(plt)
