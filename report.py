877621a48dd1ab70b6ae837edae9a9e3256995b7
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 22:18:00 +0800

    Update report.py

--- report.py
+++ report.py
@@ -665,7 +665,7 @@ elif option == '富邦悍將':
     plt.legend(loc = "best")   
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-st.pyplot(fig1)
+    st.pyplot(fig1)
   elif option1=='投手成績':
     st.header('投手成績')
     st.write(GuardiansPitching)
