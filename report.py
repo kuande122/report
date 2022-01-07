5a776fe423f480009f7b091cf5825c9f5969ab1c
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 14:20:37 +0800

    Update report.py

--- report.py
+++ report.py
@@ -120,9 +120,9 @@ if option == '中信兄弟':
         plt.title('CTBC Brothers Batting SLG VS Other Teams ') # 設定圖表標題
         plt.legend(labels=["BrothersBatting", "UnilionsBatting","DragonsBatting","GuardiansBatting","RakutenBatting"], loc = 'best')
         plt.subplots_adjust(left=0.125,
-                    bottom=0.2, 
-                    right=0.9, 
-                    top=1.0, 
+                    bottom=1.5, 
+                    right=1.5, 
+                    top=1.5, 
                     wspace=1.5, 
                     hspace=1.5)
         st.pyplot(plt)
