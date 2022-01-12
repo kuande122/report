403de49bf754c11133737cc10e8c5720a7747363
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-12 13:56:54 +0800

    Update report.py

--- report.py
+++ report.py
@@ -60,7 +60,7 @@ if option == '中信兄弟':
     st.write('地址：臺中市北屯區崇德路三段835號')
     st.write('觀眾席數：約兩萬席（內野14,321席對號座位，外野5,000席自由座位）')
     st.write('全壘打牆距離：左外野：325英呎 中外野：400英呎 右外野：325英呎')
-    image = Image.open('台中洲際棒球場.png')
+    image = Image.open('台中洲際球場.png')
     st.image(image)
   with col2:
     m1 = folium.Map(location=[24.19978, 120.68498], zoom_start=16)
