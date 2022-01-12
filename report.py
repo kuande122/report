536f7a2eb10f8dac1fb343a3cd2a252519545575
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-12 13:53:26 +0800

    Update report.py

--- report.py
+++ report.py
@@ -52,7 +52,26 @@ if option == '中信兄弟':
   st.audio(audio_file.read())  
   st.write("嗆司曲 兄弟精神")
   audio_file = open("兄弟精神.mp3", "rb")
-  st.audio(audio_file.read())  
+  st.audio(audio_file.read()) 
+  col1, col2 = st.columns(2)
+  with col1:
+    st.header('主場:臺中洲際棒球場')
+    st.write('全名：新北市立新莊棒球場（XinZhuang Baseball Stadium）')
+    st.write('地址：臺中市北屯區崇德路三段835號')
+    st.write('觀眾席數：約兩萬席（內野14,321席對號座位，外野5,000席自由座位）')
+    st.write('全壘打牆距離：左外野：325英呎 中外野：400英呎 右外野：325英呎')
+    image = Image.open('台中洲際棒球場.png')
+    st.image(image)
+  with col2:
+     m1 = folium.Map(location=[24.19978, 120.68498], zoom_start=16)
+
+    # add marker for Liberty Bell
+    tooltip = "臺中洲際棒球場"
+    folium.Marker([24.19978, 120.68498], popup="臺中洲際棒球場", tooltip=tooltip
+    ).add_to(m1)
+
+    # call to render Folium map in Streamlit
+    folium_static(m1)
     
   if option1=='球隊成績':
     st.header('球隊成績')
